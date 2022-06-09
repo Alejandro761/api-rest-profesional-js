e2522e5a81fa2dae7adfc067135f09dba3d35466
Alejandro Castaneda <alejandroxd62@gmail.com>
2022-06-08 21:20:21 -0500

    html y css de juanDC, consulta a la api de tranding: peliculas en tendencia, 5

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-console.log('ey' + API_KEY);
+const getTrendingMoviesPreview = async () => {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+
+    const movies = data.results;
+    movies.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList'); //primro debemos acceder al contenedor que tine el id trendingPreview y despues accedemos a la clase del articulo
+        
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-container');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
+
+        movieContainer.appendChild(movieImg);
+        trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+}
+
+getTrendingMoviesPreview();
